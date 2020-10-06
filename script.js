c02a08929885fa7ffae96def3f8e324f19432fef
SebasBar <toolt2y@gmail.com>
2020-10-06 09:18:10 +0200

    Added new  style, pictures and format style in css

--- script.js
+++ script.js
@@ -1,15 +1,13 @@
 function writeToFile() { 
 
- 
     var firstName = document.getElementById("first-name").value; 
     var lastName  = document.getElementById('last-name').value; 
     var country = document.getElementById('country').value;
     var subject = document.getElementById('subject').value;
 
-    const information = "First Name: " + firstName +
-    "\nLast Name: " + lastName + "\nCountry: " + country +
-    "\nSubject: " + subject;
+    const information = "First Name: " + firstName + "\nLast Name: " + lastName 
+    + "\nCountry: " + country +   "\nSubject: " + subject;
  
     var blob = new Blob([information], {type: "text/plain;charset=utf-8"});
-    saveAs(blob, toString(firstName));
+    saveAs(blob, `${toString(firstName)}.txt`);
 }
