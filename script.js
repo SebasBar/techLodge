106471e374ad54a293084042f46fcfadfe6e91a6
SebasBar <toolt2y@gmail.com>
2020-10-07 13:39:33 +0200

    Added a lot of changes of styles, functionalities and other cool stuff

--- script.js
+++ script.js
@@ -7,7 +7,47 @@ function writeToFile() {
 
     const information = "First Name: " + firstName + "\nLast Name: " + lastName 
     + "\nCountry: " + country +   "\nSubject: " + subject;
- 
+    
     var blob = new Blob([information], {type: "text/plain;charset=utf-8"});
-    saveAs(blob, `${toString(firstName)}.txt`);
+    saveAs(blob, `${firstName}.txt`);
 }
+
+function clearSelection1(){
+
+    document.getElementById('option1').checked = false;
+    document.getElementById('option2').checked = false;
+    document.getElementById('option3').checked = false;
+    document.getElementById('option4').checked = false;
+    }
+
+function clearSelection2(){
+
+    document.getElementById('option5').checked = false;
+    document.getElementById('option6').checked = false;
+    document.getElementById('option7').checked = false;
+    document.getElementById('option8').checked = false;
+     }
+
+function clearSelection3(){
+
+    document.getElementById('option9').checked = false;
+    document.getElementById('option10').checked = false;
+    document.getElementById('option11').checked = false;
+    document.getElementById('option12').checked = false;
+    }
+
+function clearSelection4(){
+
+    document.getElementById('option13').checked = false;
+    document.getElementById('option14').checked = false;
+    document.getElementById('option15').checked = false;
+    document.getElementById('option16').checked = false;
+    }
+
+function clearSelection5(){
+
+    document.getElementById('option17').checked = false;
+    document.getElementById('option18').checked = false;
+    document.getElementById('option19').checked = false;
+    document.getElementById('option20').checked = false;
+    }
